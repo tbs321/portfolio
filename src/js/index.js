2e3fcedb3d8138e4348b95a84c405122672bad6d
Talha Bin Saeed <talhabinsaeed@yahoo.com>
2021-10-10 01:54:44 +1100

    adding another pdf to the website

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,12 @@ import initTilt from "./tilt";
 import initSr from "./sr";
 import { addResume } from "./utils";
 import resume from "../assets/resume.pdf";
+import shooting from "../assets/shooting.pdf";
 
 export default function initApp() {
   initSr();
   initTilt();
   // comment this if you don't want to attach your resume
   addResume(resume);
+  addShooting(shooting);
 }
--- src/js/utils.js
+++ src/js/utils.js
@@ -4,3 +4,10 @@ export function addResume(pdf) {
   const resumeButton = document.querySelector(".cta-btn--resume");
   resumeButton.setAttribute("href", pdf);
 }
+
+export function addShooting(pdf) {
+  if (!pdf) return;
+
+  const shootingButton = document.querySelector(".cta-btn--shooting");
+  shootingButton.setAttribute("href", pdf);
+}
